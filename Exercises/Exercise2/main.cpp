e39965758178e8f87f8d3c95b4f2694950641855
SebastianMaier03 <Seb-Maier@outlook.de>
2024-05-31 16:20:53 +0200

    Impplement Minesweeper

--- Exercises/Exercise2/main.cpp
+++ Exercises/Exercise2/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "MinesweeperGrid.h"
 #include "Int2.h"
+#include <windows.h>
+void clear();
+
 int main() {
     unsigned int width = 20;
     unsigned int height = 10;
@@ -10,75 +13,92 @@ int main() {
     bool gameOver = false;
     bool gameWon = false;
 
+    HANDLE hIn;
+    INPUT_RECORD InRec;
+    DWORD NumRead;
+    DWORD fdwMode;
+
+    hIn = GetStdHandle(STD_INPUT_HANDLE);
+
+    fdwMode = ENABLE_EXTENDED_FLAGS | ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT;
+
+    if (!SetConsoleMode(hIn, fdwMode))
+        return -1;
+
+    grid.PrintGrid();
 
     while (!gameOver && !gameWon) {
-        grid.PrintGrid();
+        while (ReadConsoleInput(hIn, &InRec, 1, &NumRead) && NumRead > 0) {
+                    COORD mPos = InRec.Event.MouseEvent.dwMousePosition;
+                    DWORD mouseState = InRec.Event.MouseEvent.dwButtonState;
 
-        int x, y;
-        std::string action;
-        std::cout << "Enter coordinates to click (x y), type flag (flag x y), or type 'exit' to quit: ";
-        std::cin >> action;
+                    int x = mPos.X;
+                    int y = mPos.Y;
+                    --x;
+                    --y;
 
-        if (action == "exit") {
-            gameOver = true;
-            break;
-        }
+                    int2 clickLocation(x, y);
 
-        try {
-            if (action == "flag") {
-                std::cin >> x >> y;
-                --x;
-                --y;
-                int2 flagLocation(x, y);
-                grid.FlagCell(flagLocation);
-            } else {
-                x = std::stoi(action);
-                std::cin >> y;
-                --x;
-                --y;
-                int2 clickLocation(x, y);
-                grid.ClickCell(clickLocation);
-                if (grid.IsBomb(clickLocation)) {
-                    std::cout << "Game Over! You clicked on a bomb." << std::endl;
-                    gameOver = true;
-                } else {
-                    bool allNonBombCellsRevealed = true;
-                    for (unsigned int i = 0; i < width * height; ++i) {
-                        int2 location = grid.GetLocationFromIndex(i);
-                        if (!grid.IsBomb(location) && !grid.IsRevealed(location)) {
-                            allNonBombCellsRevealed = false;
-                            break;
-                        }
-                    }
-                    if (allNonBombCellsRevealed) {
-                        bool allBombCellsFlagged = true;
-                        for (unsigned int i = 0; i < width * height; ++i) {
-                            int2 location = grid.GetLocationFromIndex(i);
-                            if (grid.IsBomb(location) && !grid.IsCellFlagged(location)) {
-                                allBombCellsFlagged = false;
-                                break;
+                    if (mouseState & FROM_LEFT_1ST_BUTTON_PRESSED) {
+                        grid.ClickCell(clickLocation);
+                        clear();
+                        grid.PrintGrid();
+                        if (grid.IsBomb(clickLocation)) {
+                            std::cout << "Game Over! You clicked on a bomb." << std::endl;
+                            gameOver = true;
+                        } else {
+                            bool allNonBombCellsRevealed = true;
+                            for (unsigned int i = 0; i < width * height; ++i) {
+                                int2 location = grid.GetLocationFromIndex(i);
+                                if (!grid.IsBomb(location) && !grid.IsRevealed(location)) {
+                                    allNonBombCellsRevealed = false;
+                                    break;
+                                }
+                            }
+                            if (allNonBombCellsRevealed) {
+                                bool allBombCellsFlagged = true;
+                                for (unsigned int i = 0; i < width * height; ++i) {
+                                    int2 location = grid.GetLocationFromIndex(i);
+                                    if (grid.IsBomb(location) && !grid.IsCellFlagged(location)) {
+                                        allBombCellsFlagged = false;
+                                        break;
+                                    }
+                                }
+                                if (allBombCellsFlagged) {
+                                    std::cout << "Congratulations! You have won the game!" << std::endl;
+                                    gameOver = true;
+                                    gameWon = true;
+                                }
                             }
                         }
-                        if (allBombCellsFlagged) {
-                            std::cout << "Congratulations! You have won the game!" << std::endl;
-                            gameOver = true;
-                            gameWon = true;
-                        }
+                    } else if (mouseState & RIGHTMOST_BUTTON_PRESSED) {
+                        grid.FlagCell(clickLocation);
+                        grid.PrintGrid();
                     }
+                    break;
                 }
             }
-        } catch (std::exception& e) {
-            std::cout << "Invalid input. Please enter valid coordinates or type 'exit' to quit." << std::endl;
-            continue;
+        if (gameOver) {
+            std::cout << "Game Over! Revealing all bombs:" << std::endl;
+            grid.RevealAllBombs();
+            grid.PrintGrid();
+            system("pause");
         }
-    }
-
-    // Show all bombs when the game is over
-    if (gameOver) {
-        std::cout << "Game Over! Revealing all bombs:" << std::endl;
-        grid.RevealAllBombs();
-        grid.PrintGrid();
-    }
-
     return 0;
+}
+void clear() {
+    HANDLE console;
+    CONSOLE_SCREEN_BUFFER_INFO screen;
+    COORD origin;
+    DWORD chars_written;
+
+    console = GetStdHandle(STD_OUTPUT_HANDLE);
+    GetConsoleScreenBufferInfo(console, &screen);
+
+    origin.X = 0;
+    origin.Y = 0;
+    FillConsoleOutputCharacter(console, 32, screen.dwSize.X * screen.dwSize.Y, origin, &chars_written);
+    FillConsoleOutputAttribute(console, screen.wAttributes, screen.dwSize.X * screen.dwSize.Y, origin, &chars_written);
+
+    SetConsoleCursorPosition(console, origin);
 }
